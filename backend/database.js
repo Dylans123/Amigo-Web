823529eb2c6822dc1e826680b731fd69643de593
dylans123 <dylanskelly@knights.ucf.edu>
2020-03-01 18:40:38 -0500

    Built vue application

--- backend/database.js
+++ backend/database.js
@@ -15,5 +15,28 @@ client.connect(err => {
   if (err) throw err;
   else { 
     console.log('we\'re in business');
+    queryDatabase();
   }
 });
+
+function queryDatabase() {
+  const query = `
+      DROP TABLE IF EXISTS inventory;
+      CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);
+      INSERT INTO inventory (name, quantity) VALUES ('banana', 150);
+      INSERT INTO inventory (name, quantity) VALUES ('orange', 154);
+      INSERT INTO inventory (name, quantity) VALUES ('apple', 100);
+  `;
+
+  client
+      .query(query)
+      .then(() => {
+          console.log('Table created successfully!');
+          client.end(console.log('Closed client connection'));
+      })
+      .catch(err => console.log(err))
+      .then(() => {
+          console.log('Finished execution, exiting now');
+          process.exit();
+      });
+}
