960b16b34b05d91c2d4cbd0b133c77c98908e3c7
T <t@ts-macbook-pro.lan>
2020-03-07 18:46:19 -0500

    Added addUser endpoint

--- backend/database.js
+++ backend/database.js
@@ -1,11 +1,13 @@
 require('dotenv').config({path: __dirname + '/.env'})
 const pg = require('pg');
+const port = 5432;
+
 const config = {
   host: process.env['HOST'],
   user: process.env['USERNAME'],
   password: process.env['PASS'],
   database: process.env['DB'],
-  port: 5432,
+  port: port,
   ssl: false
 };
 
@@ -14,29 +16,32 @@ const client = new pg.Client(config);
 client.connect(err => {
   if (err) throw err;
   else { 
-    console.log('we\'re in business');
-    queryDatabase();
+    console.log('We\'re in business.');
+    console.log('Database running on port ' + port + '.');
   }
 });
 
-function queryDatabase() {
-  const query = `
-      DROP TABLE IF EXISTS inventory;
-      CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);
-      INSERT INTO inventory (name, quantity) VALUES ('banana', 150);
-      INSERT INTO inventory (name, quantity) VALUES ('orange', 154);
-      INSERT INTO inventory (name, quantity) VALUES ('apple', 100);
-  `;
+const addUser = (request, response) => {
+  const body = request.body;
 
+  const query = `
+    INSERT INTO "Users" ("Username", "Password", "FirstName", "LastName", "Email", "PhoneNumber", "LastLoggedIn", "Timestamp")
+    VALUES ($1, $2, $3, $4, $5, $6, NULL, NOW())
+  `;
+  
   client
-      .query(query)
-      .then(() => {
-          console.log('Table created successfully!');
-          client.end(console.log('Closed client connection'));
-      })
-      .catch(err => console.log(err))
-      .then(() => {
-          console.log('Finished execution, exiting now');
-          process.exit();
-      });
-}
+    .query(query, [body.UserName, body.Password, body.FirstName, body.LastName, body.Email, body.PhoneNumber])
+    .then(result => {
+      console.log(result);
+      response.json({'success': true, 'message': 'User has been inserted.'});
+    })
+    .catch(error => {
+      console.log(error);
+      response.json({'success': false, 'message': error.toString()});
+    })
+    .then(() => client.end());
+};
+
+module.exports = {
+  addUser
+};
