a758580ade5f4d5d2f7fb7f71907adf965f2a031
T <t@ts-macbook-pro.lan>
2020-03-09 15:37:22 -0400

    Added input validation

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,6 @@
 require('dotenv').config({path: __dirname + '/.env'});
 
-// Connect to database
+// Connect to the database.
 const pg = require('pg');
 const port = 5432;
 
@@ -23,105 +23,135 @@ client.connect(error => {
   }
 });
 
-// Sign up
+// Sign up controller
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 const key = process.env['JWT_KEY'];
 const serverURL = "localhost:3000";
+const {check, validationResult} = require('express-validator');
 
-const signup = (request, response) => {
+signup = (request, response) => {
   const body = request.body;
+  const errors = validationResult(request);
 
-  // Check to see if user exists already.
-  var query = `
-    SELECT * FROM "Users" WHERE "Username" = $1
-  `;
-  
-  client
-    .query(query, [body.Username])
-    .then(result => {
-      if (result.rows.length > 0) {
-        response.status(409);
-        response.json({'success': false, 'message': 'User already exists.'});
-      }
-      else {
-        const password = bcrypt.hashSync(body.Password, saltRounds);
+  // Input is invalid and/or username has already been taken.
+  if (!errors.isEmpty()) {
+    return response.json({'errors': errors.array()})
+  }
+  // Input is valid and username has not been taken, so add user.
+  else {
+    const password = bcrypt.hashSync(body.Password, saltRounds);
+    
+    query = `
+      INSERT INTO "Users" ("Username", "Password", "FirstName", "LastName", "Email", "PhoneNumber", "LastLoggedIn", "Timestamp")
+      VALUES ($1, $2, $3, $4, $5, $6, NULL, NOW())
+      RETURNING "UserID"
+    `;
+
+    client
+      .query(query, [body.Username, password, body.FirstName, body.LastName, body.Email, body.PhoneNumber])
+      .then(result => {
+        // Send verification request email.
+        const payload = {'body': {'Email': body.Email}};
+        sendVerificationEmail(payload);
         
-        // User does not exist, so add user.
-        query = `
-          INSERT INTO "Users" ("Username", "Password", "FirstName", "LastName", "Email", "PhoneNumber", "LastLoggedIn", "Timestamp")
-          VALUES ($1, $2, $3, $4, $5, $6, NULL, NOW())
-          RETURNING "UserID"
-        `;
+        response.json({'success': true, 'message': 'Sign up successful! Email verification request sent.'});
+      })
+      .catch(error => {
+        response.json({'success': false, 'message': error.toString()});
+      });
+  }
+};
 
-        client
-          .query(query, [body.Username, password, body.FirstName, body.LastName, body.Email, body.PhoneNumber])
-          .then(result => {
-            // Send email verification request.
-            const payload = {'host': serverURL, 'userID': result.rows[0].UserID, 'email': body.Email};
-            sendVerificationEmail(payload);
-            
-            response.json({'success': true, 'message': 'Sign up successful!'});
-          })
-          .catch(error => {
-            response.json({'success': false, 'message': error.toString()});
-          });
-      }
+// Checks to see if username already exists.
+checkUsername = (username, callback) => {
+  var query = `
+    SELECT * FROM "Users" WHERE LOWER("Username") = LOWER($1)
+  `;
+
+  client
+    .query(query, [username])
+    .then(result => {
+        callback(result.rows.length > 0);
     })
     .catch(error => {
       response.json({'success': false, 'message': error.toString()});
     });
 };
 
-// Login
-const login = (request, response) => {
-  const body = request.body;
-
+// Checks to see if email already used.
+checkEmail = (username, callback) => {
   var query = `
-    SELECT * FROM "Users" WHERE "Username" = $1
+    SELECT * FROM "Users" WHERE LOWER("Email") = LOWER($1)
   `;
-  
+
   client
-    .query(query, [body.Username])
+    .query(query, [username])
     .then(result => {
-      // Check username and password.
-      if (result.rows.length > 0 && bcrypt.compareSync(body.Password, result.rows[0].Password)) {
-        const active = result.rows[0].Active;
+        callback(result.rows.length > 0);
+    })
+    .catch(error => {
+      response.json({'success': false, 'message': error.toString()});
+    });
+};
 
-        // Check verification status.
-        if (active == true) {
-          const userID = result.rows[0].UserID;
-          
-          query = `
-            UPDATE "Users" SET "LastLoggedIn" = NOW() WHERE "UserID" = $1
-          `;
+// Login controller
+login = (request, response) => {
+  const body = request.body;
+  const errors = validationResult(request);
 
-          client
-            .query(query, [userID])
-            .then(result => {
-              const payload = {'UserID': userID};
-              const token = jwt.sign(payload, key, {expiresIn: "7d"});
-              response.json({'success': true, 'message': 'Login successful!', 'token': token, 'verified': active});
-            })
-            .catch(error => {
-              response.json({'success': false, 'message': error.toString()});
-            });
+  // Input is missing.
+  if (!errors.isEmpty()) {
+    return response.json({'errors': errors.array()})
+  }
+  // Input is not missing, so issue token.
+  else {
+    var query = `
+      SELECT * FROM "Users" WHERE LOWER("Username") = LOWER($1)
+    `;
+    
+    client
+      .query(query, [body.Username])
+      .then(result => {
+        // Check username and password.
+        if (result.rows.length > 0 && bcrypt.compareSync(body.Password, result.rows[0].Password)) {
+          const active = result.rows[0].Active;
+
+          // Check verification status.
+          if (active == true) {
+            const userID = result.rows[0].UserID;
+            
+            query = `
+              UPDATE "Users" SET "LastLoggedIn" = NOW() WHERE "UserID" = $1
+            `;
+
+            client
+              .query(query, [userID])
+              .then(result => {
+                const payload = {'UserID': userID};
+                const token = jwt.sign(payload, key, {expiresIn: "7d"});
+                response.json({'success': true, 'message': 'Login successful!', 'token': token, 'verified': active});
+              })
+              .catch(error => {
+                response.json({'success': false, 'message': error.toString()});
+              });
+          }
+          else {
+            response.json({'success': false, 'message': 'Email has not been verified.', 'verified': active});
+          }
         }
         else {
-          response.json({'success': false, 'message': 'Login in failed.', 'verified': active});
+          response.json({'success': false, 'message': 'Login failed.'});
         }
-      }
-      else {
-        response.json({'success': false, 'message': 'Login in failed.'});
-      }
-    })
-    .catch(error => {
-      response.json({'success': false, 'message': error.toString()});
-    });
+      })
+      .catch(error => {
+        response.json({'success': false, 'message': error.toString()});
+      });
+  }
 };
 
-// Validate user
+// Validates a user.
 validateUser = (request, response, next) => {
   jwt.verify(request.headers['x-access-token'], key, (error, decoded) => {
     if (error) {
@@ -133,13 +163,13 @@ validateUser = (request, response, next) => {
   });
 }
 
-// Email verification
+// Email verification controllers
 const nodemailer = require("nodemailer");
 const mailUsername = process.env['MAIL_USERNAME'];
 const mailPassword = process.env['MAIL_PASSWORD'];
 const verificationKey = process.env['EMAIL_VERIFIER_JWT_KEY'];
 
-var smtpTransport = nodemailer.createTransport({
+const smtpTransport = nodemailer.createTransport({
   service: "gmail",
   auth: {
       user: mailUsername,
@@ -147,25 +177,43 @@ var smtpTransport = nodemailer.createTransport({
   }
 });
 
-sendVerificationEmail = (body) => {
-  const payload = {'UserID': body.userID, 'Email': body.email};
-  const token = jwt.sign(payload, verificationKey, {expiresIn: "3d"});
-  const link = "http://" + body.host + "/verify?token=" + token;
+sendVerificationEmail = (request, response) => {
+  const body = request.body;
 
-  const mailOptions = {
-      'to': body.email,
-      'subject': "Amigo: Verify your email address",
-      'html': "Hello,<br><br>Please click on the link to verify your email.<br><a href=" + link + ">Click here to verify</a>"
-  }
+  const query = `
+    SELECT * FROM "Users" WHERE LOWER("Email") = LOWER($1) AND "Active" = false
+  `;
+  
+  client
+    .query(query, [body.Email])
+    .then(result => {
+      if (result.rows.length > 0) {
+        const payload = {'Email': body.Email};
+        const token = jwt.sign(payload, verificationKey, {expiresIn: "3d"});
+        const link = "http://" + serverURL + "/verify?token=" + token;
 
-  smtpTransport.sendMail(mailOptions, (error, response) => {
-    if (error) {
+        const mailOptions = {
+            'to': body.Email,
+            'subject': "Amigo: Verify your email address",
+            'html': "Hello,<br><br>Please click on the link to verify your email.<br><a href=" + link + ">Click here to verify</a>"
+        }
+
+        smtpTransport.sendMail(mailOptions, (error, transportResponse) => {
+          if (error) {
+            response.json({'success': false, 'message': error.toString()});
+          }
+          else {
+            response.json({'success': true, 'message': 'Verification request email sent.'});
+          }
+        });
+      }
+      else {
+        response.json({'success': false, 'message': 'Email already verified or not registered.'});
+      }
+    })
+    .catch(error => {
       response.json({'success': false, 'message': error.toString()});
-    }
-    else {
-      response.json({'success': true, 'message': 'Verification request email sent.'});
-    }
-  });
+    });
 };
 
 verifyEmail = (request, response) => {
@@ -178,18 +226,34 @@ verifyEmail = (request, response) => {
     else {
       const payload = jwt.decode(token);
 
-      const query = `
-        UPDATE "Users" SET "Active" = true WHERE "UserID" = $1
+      var query = `
+        SELECT "Active" FROM "Users" WHERE LOWER("Email") = $1 AND "Active" = false
       `;
       
       client
-        .query(query, [payload.UserID])
+        .query(query, [payload.Email])
         .then(result => {
           if (result.rowCount > 0) {
-            response.json({'success': true, 'message': 'Email verified successfully.'});
+            query = `
+              UPDATE "Users" SET "Active" = true WHERE LOWER("Email") = LOWER($1)
+            `;
+            
+            client
+              .query(query, [payload.Email])
+              .then(result => {
+                if (result.rowCount > 0) {
+                  response.json({'success': true, 'message': 'Email verified successfully.'});
+                }
+                else {
+                  response.json({'success': false, 'message': 'Problem with email verification.'});
+                }
+              })
+              .catch(error => {
+                response.json({'success': false, 'message': error.toString()});
+              });
           }
           else {
-            response.json({'success': false, 'message': 'Problem with email verification.'});
+            response.json({'success': false, 'message': 'Email already verified or not registered.'});
           }
         })
         .catch(error => {
@@ -199,9 +263,8 @@ verifyEmail = (request, response) => {
   });
 };
 
-// Dispay user information
-const userInfo = (request, response) => {
-  const body = request.body;
+// Dispay user information controller
+userInfo = (request, response) => {
   const payload = jwt.decode(request.headers['x-access-token']);
 
   const query = `
@@ -227,6 +290,8 @@ const userInfo = (request, response) => {
 
 module.exports = {
   signup,
+  checkUsername,
+  checkEmail,
   login,
   validateUser,
   sendVerificationEmail,
