a60b8387f81aef14986a6b48b4d5ec4841042e1a
dylans123 <dylanskelly@knights.ucf.edu>
2020-03-29 01:52:26 -0400

    Created table and input some dummy data

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -12,14 +12,47 @@
           <h3><b>Groups</b></h3>
           <button type="button" class="btn btn-outline-primary" v-on:click="logout()">New Group +</button>
         </div>
+        <div class="row">
+          <div class="col-3">
+            
+          </div>
+          <div class="col-9">
+            <md-table>
+              <md-table-row>
+                <md-table-head>First Name</md-table-head>
+                <md-table-head>Last Name</md-table-head>
+                <md-table-head>Display Name</md-table-head>
+                <md-table-head>Date Joined</md-table-head>
+                <md-table-head></md-table-head>
+              </md-table-row>
+              <template v-for="user in users">
+                <md-table-row :key="user.displayName">
+                  <md-table-cell>{{ user.firstName }}</md-table-cell>
+                  <md-table-cell>{{ user.lastName }}</md-table-cell>
+                  <md-table-cell>{{ user.displayName }}</md-table-cell>
+                  <md-table-cell>{{ user.dateJoined }}</md-table-cell>
+                  <md-table-cell>
+                    <button type="button" class="btn btn-outline-primary" v-on:click="logout()">New Group +</button>
+                  </md-table-cell>
+                </md-table-row>
+              </template>
+            </md-table>
+          </div>
+        </div>
       </div>
     </div>
 </template>
 <script>
+import generateUsers from "../users.js"
 export default {
   mounted: () => {
     const cookie = document.cookie;
     console.log(cookie.john);
+  },
+  data: function() {
+    return {
+      users: generateUsers()
+    }
   },
   methods: {
     logout: function() {
---
+++ frontend/src/users.js
@@ -0,0 +1,169 @@
+function generateUsers() {
+return (
+  [
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    },
+    {
+      "firstName": "Dylan",
+      "lastName": "Skelly",
+      "displayName": "dylanskelly",
+      "dateJoined": "February 5th, 2020"
+    }
+  ]
+)}
+
+export default generateUsers;
