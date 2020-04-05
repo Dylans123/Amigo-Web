41860b3be7573eef0e6662e0e78bac8ce5a35ea2
capelor <caiolou@hotmail.com>
2020-04-04 20:58:55 -0400

    made changes to register, login, register, and made new forgot password and new password

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,11 +1,17 @@
 import Home from './components/Home.vue';
 import Register from './components/Register.vue';
 import Login from './components/Login.vue';
+import Forgot from './components/Forgot.vue';
+import Newpassword from './components/Newpassword.vue';
+
 
 const routes = [
     { path: '/', component: Home },
     { path: '/register', component: Register },
+    { path: '/forgot', component: Forgot },
     { path: '/login', component: Login },
+    { path: '/newpassword', component: Newpassword },
+    
 ];
 
 export default routes;
