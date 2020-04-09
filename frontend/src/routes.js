78bb0380fb3ce56cd9b3fe42da31dfbf2d168644
dylans123 <dylanskelly@knights.ucf.edu>
2020-04-09 00:31:22 -0400

    Changed routing so that groups is routed to the /groups oute

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,11 +4,11 @@ import Login from './components/Login.vue';
 import Forgot from './components/Forgot.vue';
 import Newpassword from './components/Newpassword.vue';
 import Verify from './components/Verify.vue';
-import Home from './components/Home.vue';
+import Groups from './components/Groups.vue';
 
 
 const routes = [
-    { path: '/', component: Home },
+    { path: '/groups', component: Groups },
     { path: '/register', component: Register },
     { path: '/forgot', component: Forgot },
     { path: '/login', component: Login },
