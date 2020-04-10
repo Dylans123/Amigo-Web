b58051f79db0fda72454b3f4f8af194611e7caa8
dylans123 <dylanskelly@knights.ucf.edu>
2020-04-09 21:38:30 -0400

    Populated placeholders on dashboard with some styling and data

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,26 +1,45 @@
 <template>
   <div class="container">
-    <div class="row justify-content-center align-items-center" style="height: 80vh;">
+    <div class="row justify-content-center align-items-center" style="height: 90vh;">
       <div class="col-3">
         <md-card class="md-elevation-15" style="height: 25vh; background-color: white">
-          <div></div>
+          <div style="width: 100%; height: 100%; display: flex; align-items: center; justify-content: center; flex-direction: column">
+            <h5>Number of Users</h5>
+            <h1>101</h1>
+          </div>
         </md-card>
       </div>
       <div class="col-3">
         <md-card class="md-elevation-15" style="height: 25vh; background-color: white">
+          <div style="width: 100%; height: 100%; display: flex; align-items: center; justify-content: center; flex-direction: column">
+            <h5>Number of Channels</h5>
+            <h1>101</h1>
+          </div>
         </md-card>
       </div>
       <div class="col-3">
         <md-card class="md-elevation-15" style="height: 25vh; background-color: white">
+          <div style="width: 100%; height: 100%; display: flex; align-items: center; justify-content: center; flex-direction: column">
+            <h5>Total Direct Messages</h5>
+            <h1>101</h1>
+          </div>
         </md-card>
       </div>
       <div class="col-3">
-        <md-card class="md-elevation-15" style="height: 25vh; background-color: white">
+        <md-card class="md-elevation-15" style="height: 25vh; background-color: white; text-align: center">
+          <div style="width: 100%; height: 100%; display: flex; align-items: center; justify-content: center; flex-direction: column">
+            <h5>Number of Channel Messages</h5>
+            <h1>101</h1>
+          </div>
         </md-card>
       </div>
       <div class="col-12">
         <md-card class="md-elevation-15" style="height: 50vh; background-color: white">
-
+          <md-card-header>
+            <md-card-header-text>
+              <div><h6>Messages Sent Over Time</h6></div>
+            </md-card-header-text>
+          </md-card-header>
         </md-card>
       </div>
     </div>
