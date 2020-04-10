ccf9bbc23d661d25bb6fa9839568bf0fa030d216
dylans123 <dylanskelly@knights.ucf.edu>
2020-04-09 21:29:34 -0400

    Added basic styling to the dashboard

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,9 +1,34 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div class="row justify-content-center align-items-center" style="height: 80vh;">
+      <div class="col-3">
+        <md-card class="md-elevation-15" style="height: 25vh; background-color: white">
+          <div></div>
+        </md-card>
+      </div>
+      <div class="col-3">
+        <md-card class="md-elevation-15" style="height: 25vh; background-color: white">
+        </md-card>
+      </div>
+      <div class="col-3">
+        <md-card class="md-elevation-15" style="height: 25vh; background-color: white">
+        </md-card>
+      </div>
+      <div class="col-3">
+        <md-card class="md-elevation-15" style="height: 25vh; background-color: white">
+        </md-card>
+      </div>
+      <div class="col-12">
+        <md-card class="md-elevation-15" style="height: 50vh; background-color: white">
+
+        </md-card>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
-    
+  
 }
 </script>
 <style scoped>
