1f4f2a3f296c3ea0cb9c7a0281fa3e57c1a35487
dylans123 <dylanskelly@knights.ucf.edu>
2020-04-11 03:14:29 -0400

    Restructured routing and added a home component

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,4 +1,4 @@
-// import Home from './components/Home.vue';
+import Home from './components/Home.vue';
 import Register from './components/Register.vue';
 import Login from './components/Login.vue';
 import Forgot from './components/Forgot.vue';
@@ -26,39 +26,24 @@ if (!jwt) {
 }
 
 const routes = [
-  { path: '/', component: Wrapper,
-    children: [
-      {path: '', component: Dashboard, props: { jwt: jwt }}
-    ]
-  },
-  { path: '/groups/', component: Wrapper,
-      children: [
-        {path: 'edit', component: EditGroups, props: { jwt: jwt }},
-        {path: 'create', component: CreateGroups, props: { jwt: jwt }},
-      ]
-  },
-  { path: '/users/', component: Wrapper,
-      children: [
-        {path: 'view', component: ViewUsers, props: { jwt: jwt }},
-        {path: 'create', component: CreateUser, props: { jwt: jwt }},
-      ]
-  },
-  { path: '/tags/', component: Wrapper,
-      children: [
-        {path: 'edit', component: EditTags, props: { jwt: jwt }},
-        {path: 'create', component: CreateTags, props: { jwt: jwt }},
-      ]
-  },
-  { path: '/administrators/', component: Wrapper,
-      children: [
-        {path: 'edit', component: EditAdministrators, props: { jwt: jwt }}
-      ]
-  },
+  { path: '/', component: Home},
   { path: '/register', component: Register },
   { path: '/forgot', component: Forgot },
   { path: '/login', component: Login },
   { path: '/newpassword', component: Newpassword },
-  { path: '/verify', component: Verify }
+  { path: '/verify', component: Verify },
+  { path: '/admin', component: Wrapper, 
+    children: [
+      {path: '/', component: Dashboard, props: { jwt: jwt }},
+      {path: 'groups/edit', component: EditGroups, props: { jwt: jwt }},
+      {path: 'groups/create', component: CreateGroups, props: { jwt: jwt }},
+      {path: 'users/view', component: ViewUsers, props: { jwt: jwt }},
+      {path: 'users/create', component: CreateUser, props: { jwt: jwt }},
+      {path: 'tags/edit', component: EditTags, props: { jwt: jwt }},
+      {path: 'tags/create', component: CreateTags, props: { jwt: jwt }},
+      {path: 'administrators/edit', component: EditAdministrators, props: { jwt: jwt }}
+    ]
+  }
 ];
 
 export default routes;
